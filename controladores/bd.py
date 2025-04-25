b428b9cebb50349033c3df2e84712679eb587d6e
fabss269 <fabianapm060126@gmail.com>
2025-04-24 20:47:07 -0500

    buaaaaaaaaaaa

--- controladores/bd.py
+++ controladores/bd.py
@@ -3,8 +3,8 @@ from pymysql.cursors import DictCursor
 
 def obtener_conexion():
     return pymysql.connect(host='localhost',
-                                port=3306,
-                                # port=3307,
+                                # port=3306,
+                                port=3307,
                                 user='root',
                                 password='',
                                 db='bd_encomiendas' ,
